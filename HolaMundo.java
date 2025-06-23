dac52d239d791cbec5a4fcc0b5b072e780be4555
Maxi <mdd.bsd@gmail.com>
2025-06-23 10:54:22 -0300

    commit inicial

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo{
+    public static void main(String[] args){
+        System.out.print("Hola Mundo ");
+        System.out.print("Ingrese su nombre: ");
+        String nombre = System.console().readLine();
+    }
+}
